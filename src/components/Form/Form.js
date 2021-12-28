3b743ffb0dc1a89d9196b3f85af7d4f29ba4a1e3
Diego <newskyforge2@hotmail.com>
2021-12-28 11:07:48 -0600

    Add textarea to form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,16 +3,29 @@ import React, { Component } from 'react';
 export class Form extends Component {
   render() {
     let fields = this.props.fields.map(
-      ({ type, placeholder, value, name }, index) => (
-        <input
-          type={type}
-          placeholder={placeholder}
-          value={value}
-          name={name}
-          key={index}
-          onChange={(e) => this.props.onChange(name, e.target.value)}
-        />
-      )
+      ({ type, placeholder, value, name }, index) => {
+        if (type === 'textarea')
+          return (
+            <textarea
+              placeholder={placeholder}
+              value={value}
+              name={name}
+              key={index}
+              onChange={(e) => this.props.onChange(name, e.target.value)}
+            />
+          );
+        else
+          return (
+            <input
+              type={type}
+              placeholder={placeholder}
+              value={value}
+              name={name}
+              key={index}
+              onChange={(e) => this.props.onChange(name, e.target.value)}
+            />
+          );
+      }
     );
 
     return <div>{fields}</div>;
