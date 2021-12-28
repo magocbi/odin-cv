46e25601fc79291e23575b226435b3d88cfa483d
Diego <newskyforge2@hotmail.com>
2021-12-28 11:19:38 -0600

    Add work info form  and its  tests

--- src/components/CVEdit/CVEdit.js
+++ src/components/CVEdit/CVEdit.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import GeneralInfoForm from '../GeneralInfoForm/GeneralInfoForm';
 import EducationInfoForm from '../EducationalInfoForm/EducationalInfoForm';
+import WorkInfoForm from '../WorkInfoForm/WorkInfoForm';
 
 export default class CVEdit extends Component {
   render() {
     const {
       generalInformation,
       educationalList,
+      workList,
       onEducationDelete,
       onEducationAddition,
+      onWorkDelete,
+      onWorkAddition,
     } = this.props;
 
     const educationalFormList = educationalList.map((education) => (
@@ -19,6 +23,14 @@ export default class CVEdit extends Component {
         key={education.id}
       />
     ));
+    const workFormList = workList.map((work) => (
+      <WorkInfoForm
+        fields={work}
+        id={work.id}
+        onDelete={onWorkDelete}
+        key={work.id}
+      />
+    ));
     return (
       <div>
         <GeneralInfoForm fields={generalInformation} />
@@ -26,6 +38,8 @@ export default class CVEdit extends Component {
         <button onClick={onEducationAddition}>
           Add Educational Experience
         </button>
+        {workFormList}
+        <button onClick={onWorkAddition}>Add Work Experience</button>
       </div>
     );
   }
--- src/components/CVEdit/__test__/CVEdit.test.js
+++ src/components/CVEdit/__test__/CVEdit.test.js
@@ -23,23 +23,48 @@ function getEducationInfo() {
   return educationalInfo;
 }
 
+function getWorkInfo() {
+  const workInfo = {
+    companyName: 'MyCompanyName',
+    position: 'myposition',
+    from: '11-11-2011',
+    to: '11-11-2015',
+    description: 'My tasks were 1,2,3 and 4',
+    id: count,
+  };
+  count++;
+  return workInfo;
+}
+
 describe('CVEdit', () => {
   test('should render general information input from props', () => {
-    render(<CVEdit generalInformation={generalInfo} educationalList={[]} />);
+    render(
+      <CVEdit
+        generalInformation={generalInfo}
+        educationalList={[]}
+        workList={[]}
+      />
+    );
     const inputElements = screen.getAllByDisplayValue(
       /[(myName)(email@email)(55555555)(myAdress)]/i
     );
     expect(inputElements.length).toBe(4);
   });
+
   test('should render educational information input from props', () => {
     render(
-      <CVEdit generalInformation={{}} educationalList={[getEducationInfo()]} />
+      <CVEdit
+        generalInformation={{}}
+        educationalList={[getEducationInfo()]}
+        workList={[]}
+      />
     );
     const inputElements = screen.getAllByDisplayValue(
       /[(MySchoolName)(mydegree)(11-11-2011)(11-11-2015)]/i
     );
     expect(inputElements.length).toBe(4);
   });
+
   test('should render multiple educational information input from props', () => {
     render(
       <CVEdit
@@ -49,6 +74,7 @@ describe('CVEdit', () => {
           getEducationInfo(),
           getEducationInfo(),
         ]}
+        workList={[]}
       />
     );
     const inputElements = screen.getAllByDisplayValue(
@@ -56,6 +82,7 @@ describe('CVEdit', () => {
     );
     expect(inputElements.length).toBe(12);
   });
+
   test('should call callback to add educational experience', () => {
     const onEducationAdditionMocked = jest.fn();
     render(
@@ -63,10 +90,54 @@ describe('CVEdit', () => {
         generalInformation={{}}
         educationalList={[]}
         onEducationAddition={onEducationAdditionMocked}
+        workList={[]}
       />
     );
     const educationAddBtn = screen.getByText(/Add Education/i);
     userEvent.click(educationAddBtn);
     expect(onEducationAdditionMocked).toHaveBeenCalledTimes(1);
+  });
+
+  test('should render work information input from props', () => {
+    render(
+      <CVEdit
+        generalInformation={{}}
+        educationalList={[]}
+        workList={[getWorkInfo()]}
+      />
+    );
+    const inputElements = screen.getAllByDisplayValue(
+      /[(MyCompanyName)(myposition)(11-11-2011)(11-11-2015)(My tasks were 1,2,3 and 4)]/i
+    );
+    expect(inputElements.length).toBe(5);
+  });
+
+  test('should render multiple work information input from props', () => {
+    render(
+      <CVEdit
+        generalInformation={{}}
+        educationalList={[]}
+        workList={[getWorkInfo(), getWorkInfo(), getWorkInfo()]}
+      />
+    );
+    const inputElements = screen.getAllByDisplayValue(
+      /[(MyCompanyName)(myposition)(11-11-2011)(11-11-2015)(My tasks were 1,2,3 and 4)]/i
+    );
+    expect(inputElements.length).toBe(15);
+  });
+
+  test('should call callback to add work experience', () => {
+    const onWorkAdditionMocked = jest.fn();
+    render(
+      <CVEdit
+        generalInformation={{}}
+        educationalList={[]}
+        onWorkAddition={onWorkAdditionMocked}
+        workList={[]}
+      />
+    );
+    const workAddBtn = screen.getByText(/Add Work/i);
+    userEvent.click(workAddBtn);
+    expect(onWorkAdditionMocked).toHaveBeenCalledTimes(1);
   });
 });
