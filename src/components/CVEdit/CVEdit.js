2d8fdb9ce77075858335163991cadd1489b84a03
Diego <newskyforge2@hotmail.com>
2021-12-27 19:46:00 -0600

    Add educational information form  and its tests

--- src/components/CVEdit/CVEdit.js
+++ src/components/CVEdit/CVEdit.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import GeneralInfoForm from '../GeneralInfoForm/GeneralInfoForm';
+import EducationInfoForm from '../EducationalInfoForm/EducationalInfoForm';
 
 export default class CVEdit extends Component {
   render() {
-    const { generalInformation } = this.props;
+    const {
+      generalInformation,
+      educationalList,
+      onEducationDelete,
+      onEducationAddition,
+    } = this.props;
 
+    const educationalFormList = educationalList.map((education) => (
+      <EducationInfoForm
+        fields={education}
+        id={education.id}
+        onDelete={onEducationDelete}
+        key={education.id}
+      />
+    ));
     return (
       <div>
         <GeneralInfoForm fields={generalInformation} />
+        {educationalFormList}
+        <button onClick={onEducationAddition}>
+          Add Educational Experience
+        </button>
       </div>
     );
   }
--- src/components/CVEdit/__test__/CVEdit.test.js
+++ src/components/CVEdit/__test__/CVEdit.test.js
@@ -1,18 +1,72 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import CVEdit from '../CVEdit';
+
+const generalInfo = {
+  name: 'myName',
+  email: 'email@email',
+  phoneNumber: '55555555',
+  address: 'myAdress',
+};
+
+let count = 1;
+
+function getEducationInfo() {
+  const educationalInfo = {
+    schoolName: 'MySchoolName',
+    degree: 'mydegree',
+    from: '11-11-2011',
+    to: '11-11-2015',
+    id: count,
+  };
+  count++;
+  return educationalInfo;
+}
 
 describe('CVEdit', () => {
   test('should render general information input from props', () => {
-    const generalInfo = {
-      name: 'myName',
-      email: 'email@email',
-      phoneNumber: '55555555',
-      address: 'myAdress',
-    };
-    render(<CVEdit generalInformation={generalInfo} />);
+    render(<CVEdit generalInformation={generalInfo} educationalList={[]} />);
     const inputElements = screen.getAllByDisplayValue(
       /[(myName)(email@email)(55555555)(myAdress)]/i
     );
     expect(inputElements.length).toBe(4);
+  });
+  test('should render educational information input from props', () => {
+    render(
+      <CVEdit generalInformation={{}} educationalList={[getEducationInfo()]} />
+    );
+    const inputElements = screen.getAllByDisplayValue(
+      /[(MySchoolName)(mydegree)(11-11-2011)(11-11-2015)]/i
+    );
+    expect(inputElements.length).toBe(4);
+  });
+  test('should render multiple educational information input from props', () => {
+    render(
+      <CVEdit
+        generalInformation={{}}
+        educationalList={[
+          getEducationInfo(),
+          getEducationInfo(),
+          getEducationInfo(),
+        ]}
+      />
+    );
+    const inputElements = screen.getAllByDisplayValue(
+      /[(MySchoolName)(mydegree)(11-11-2011)(11-11-2015)]/i
+    );
+    expect(inputElements.length).toBe(12);
+  });
+  test('should call callback to add educational experience', () => {
+    const onEducationAdditionMocked = jest.fn();
+    render(
+      <CVEdit
+        generalInformation={{}}
+        educationalList={[]}
+        onEducationAddition={onEducationAdditionMocked}
+      />
+    );
+    const educationAddBtn = screen.getByText(/Add Education/i);
+    userEvent.click(educationAddBtn);
+    expect(onEducationAdditionMocked).toHaveBeenCalledTimes(1);
   });
 });
